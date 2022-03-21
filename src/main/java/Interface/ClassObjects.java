990aa7249aee32c33b7443b75d61800bfded0c1d
Meet Shah <meshah@mobiquityinc.com>
2022-03-21 16:36:40 +0530

    First commit

---
+++ src/main/java/Interface/ClassObjects.java
@@ -0,0 +1,10 @@
+package Interface;
+
+import Locators.DashboardScreen;
+import Locators.TopicSelection;
+
+
+public interface ClassObjects {
+    DashboardScreen dashboardScreen = new DashboardScreen();
+
+}
